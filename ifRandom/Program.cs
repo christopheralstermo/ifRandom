28ccae96ac44a8fe0ef5c149d603018a3befa1dc
Christopher Alstermo <christopher_alstermo@hotmail.com>
2025-05-08 11:21:42 +0200

    Add project files.

---
+++ ifRandom/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ifRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            bool theSwitch = true;
+            
+            while(theSwitch)
+            {
+                var nr1 = rand.Next(1, 30);
+                var nr2 = rand.Next(1, 30);
+                Console.ReadKey();
+                
+                if(nr1 != nr2)
+                {
+                    Console.WriteLine($"nr1: {nr1}\n\nnr2: {nr2}\n\n");
+                }
+                else
+                {
+                    Console.WriteLine($"De er like! og nr som matchet var {nr1}");
+                    Console.ReadKey();
+                    Console.ReadKey();
+                    theSwitch = false;
+                }
+            }
+            
+        }
+    }
+}
